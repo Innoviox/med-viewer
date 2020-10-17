8074bf182772347fbf2d96406d383d7d3fa3e54f
Rajen Dey <rajen.dey@berkeley.edu>
2020-10-17 14:47:49 -0400

    update a little

--- tfidf_sample.py
+++ tfidf_sample.py
@@ -2,6 +2,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import os
 
 def make_filenams():
-    all_files = ""
+    all_files = []
     path = "./where_the_txt_files_are"
-    
+    dirs = os.listdir(path)
+    for file in dirs:
+        all_files.append(path + "/" + file)
+    return all_files
+
