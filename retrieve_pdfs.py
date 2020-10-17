eee37bd022a4b8759d1d7622d248587445cfad02
Zach Zhao <thezachzhao@gmail.com>
2020-10-17 11:56:03 -0400

    basic api access

--- retrieve_pdfs.py
+++ retrieve_pdfs.py
@@ -1,2 +1,11 @@
-
 import requests
+import json
+
+r = requests.get('https://api.biorxiv.org/covid19/0')
+content_dict = json.loads(r.content)
+
+
+# Retrieving abstracts is easy - PDFs are hard to obtain
+for paper in content_dict["collection"]:
+    print(paper["rel_title"])
+    print(paper["rel_abs"])
