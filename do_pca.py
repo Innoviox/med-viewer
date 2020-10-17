ef23ffe75707153e5788aad7665b60b6644c9e6e
Rajen Dey <rajen.dey@berkeley.edu>
2020-10-17 16:23:22 -0400

    do pca on the sample papers

--- do_pca.py
+++ do_pca.py
@@ -1,11 +1,12 @@
 from sklearn.decomposition import PCA
 import pandas as pd
 
-csv_file = "tfidf_abstracts.csv"
+csv_file = "papers.csv"
 df = pd.read_csv(csv_file)
 df = df.iloc[:, 1:]  # don't use ids
-n_components = 20
+n_components = 100
 pca = PCA(n_components=n_components)
 pca.fit(df)
-pd.DataFrame(pca.transform(df), columns=['PCA%i' % i for i in range(n_components)], index=df.index)
-print(sum(pca.explained_variance_ratio_))   
+pca_df = pd.DataFrame(pca.transform(df), columns=['PCA%i' % i for i in range(n_components)], index=df.index)
+pca_df.to_csv("papers_after_pca.csv")
+print(sum(pca.explained_variance_ratio_))
